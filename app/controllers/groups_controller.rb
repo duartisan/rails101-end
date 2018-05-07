9d514a4d43cef6432f105b51107958012bea4823
段郑阳 <duanzhengyang@duanzhengyangdeMacBook-Pro.local>
2018-05-07 19:12:38 +0800

    add permission check on show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -24,10 +25,7 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "您没有权限进行该操作！"
-    end
+    find_group_and_check_permission
     if @group.update(group_params)
       redirect_to groups_path, notice: "更新成功！"
     else
@@ -36,23 +34,24 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "您没有权限进行该操作！"
-    end
+    find_group_and_check_permission
     @group.destroy
     flash[:alert] = "删除成功！"
     redirect_to groups_path
   end
 
   def edit
-    @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "您没有权限访进行该操作！"
-    end
+    find_group_and_check_permission
   end
 
   private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "您没有权限进行该操作！"
+    end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
